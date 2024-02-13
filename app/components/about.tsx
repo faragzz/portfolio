a5528babf1457ccef9d1130f4ad2ddfb8cc58e71
faragzz <a.khaled46462@gmail.com>
2024-02-14 01:29:52 +0200

    added my name

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -23,6 +23,7 @@ export default function About({}: Props) {
     <div className="flex flex-col xl:flex-row justify-between items-center w-full mt-32">
       <div className="flex flex-col text-black ml-12 mr-10">
         <div className="flex items-center">
+          <br/>
           <p className="text-5xl text-black ml-4">I'm a </p>
           <span className="font-bold text-5xl text-black ml-4">{text}</span>
           <span className="text-black text-5xl">
@@ -31,10 +32,7 @@ export default function About({}: Props) {
         </div>
         <br />
         <div className="text-md">
-          I am a passionate and ambitious developer with extensive experience in
-          building web, mobile, and backend applications .With a solid
-          foundation in React, Next.js, Kotlin, Flutter, React Native, and
-          NestJS, I am well-equipped to handle diverse projects and challenges.
+        I am Ahmed Khaled, a passionate and ambitious developer with extensive experience in building web, mobile, and backend applications. With a solid foundation in React, Next.js, Kotlin, Flutter, React Native, and NestJS, I am well-equipped to handle diverse projects and challenges.
         </div>
       </div>
       <div className="mr-12">
