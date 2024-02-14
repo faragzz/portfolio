b35d908a1e8dfde655a44c40a1053c4e1e97eeb6
faragzz <a.khaled46462@gmail.com>
2024-02-14 21:47:55 +0200

    display fix

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -27,11 +27,11 @@ export default function About({}: Props) {
       <div className="flex flex-col text-black ml-12 mr-10">
         <div className="flex items-center">
           <br />
-          <p className=" xl:text-5xl text-4xl text-black ml-4">I&apos;m a </p>
-          <span className="font-bold  text-4xl xl:text-5xl text-black ml-4">
+          <p className=" text-2xl xl:text-5xl text-black ml-4">I&apos;m a </p>
+          <span className="font-bold text-2xl xl:text-5xl text-black ml-4">
             {text}
           </span>
-          <span className="text-black text-5xl">
+          <span className="text-black xl:text-5xl">
             <Cursor />
           </span>
         </div>
